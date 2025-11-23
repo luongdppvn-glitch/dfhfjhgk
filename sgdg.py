dd0cd067001b5aa6a7668c1d17706caa2c940a39
luongdppvn-glitch <luongdppvn@gmail.com>
2025-11-23 10:42:13 +0700

    Update sgdg.py

--- sgdg.py
+++ sgdg.py
@@ -1,1 +1,7 @@
-
+with open(full_path, "r", encoding="utf-8") as f:
+                        content = f.read()
+                    if search_text in content:
+                        content = content.replace(search_text, replace_text)
+                        with open(full_path, "w", encoding="utf-8") as f:
+                            f.write(content)
+                        msg = f"✅ Đã sửa file: {full_path}"
